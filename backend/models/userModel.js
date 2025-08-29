b6227c734ad1cdd6f08aee3f181af52e58572bbd
RithwikaNagula <155565997+RithwikaNagula@users.noreply.github.com>
2025-08-29 23:48:34 +0530

    Schema modified

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,35 +5,18 @@ const userSchema = new mongoose.Schema(
     role: {
       type: String,
       required: [true, "role is required"],
-      enum: ["admin", "organisation", "donar", "hospital"],
+      enum: ["admin","Supplier"],
     },
     name: {
       type: String,
       required: function () {
-        if (this.role === "user" || this.role === "admin") {
+        if (this.role === "Supplier" || this.role === "admin") {
           return true;
         }
         return false;
       },
     },
-    organisationName: {
-      type: String,
-      required: function () {
-        if (this.role === "organisation") {
-          return true;
-        }
-        return false;
-      },
-    },
-    hospitalName: {
-      type: String,
-      required: function () {
-        if (this.role === "hospital") {
-          return true;
-        }
-        return false;
-      },
-    },
+    
     email: {
       type: String,
       required: [true, "email is required"],
@@ -43,9 +26,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, "password is requied"],
     },
-    website: {
-      type: String,
-    },
+   
     address: {
       type: String,
       required: [true, "address is required"],
