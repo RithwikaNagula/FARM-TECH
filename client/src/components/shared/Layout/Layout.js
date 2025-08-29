ddc146c9193471f803d0024a9b97d6ed5afc1224
RithwikaNagula <155565997+RithwikaNagula@users.noreply.github.com>
2025-08-29 23:24:36 +0530

    Update Layout.js

--- client/src/components/shared/Layout/Layout.js
+++ client/src/components/shared/Layout/Layout.js
@@ -8,10 +8,7 @@ const Layout = ({ children }) => {
       <div className="header">
         <Header />
       </div>
-      <div className="row g-0">
-        <div className="col-md-3">
-          <Sidebar />
-        </div>
+      
         <div className="col-md-9">{children}</div>
       </div>
     </>
