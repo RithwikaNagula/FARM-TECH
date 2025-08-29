819879331fac09f626e50ff3b25198df8b845491
RithwikaNagula <155565997+RithwikaNagula@users.noreply.github.com>
2025-08-29 23:40:56 +0530

    Updated Routes

--- client/src/App.js
+++ client/src/App.js
@@ -10,29 +10,18 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ProtectedRoute from "./components/Routes/ProtectedRoute";
 import PublicRoute from "./components/Routes/PublicRoute";
-import Donar from "./pages/Dashboard/Donar";
-import Hospitals from "./pages/Dashboard/Hospitals";
-import OrganisationPage from "./pages/Dashboard/OrganisationPage";
-import Consumer from "./pages/Dashboard/Consumer";
-import Donation from "./pages/Donation";
 import Analytics from "./pages/Dashboard/Analytics";
-import DonarList from "./pages/Admin/DonarList";
-import HospitalList from "./pages/Admin/HospitalList";
-import OrgList from "./pages/Admin/OrgList";
+
 import AdminHome from "./pages/Admin/AdminHome";
 import Contact from "./pages/contactus";
-import Navbar from "./pages/Navbar";
 import Footer from "./pages/Footer";
-import AboutUs from "./pages/AboutUs";
 import VolunteerForm from "./pages/Volunteer";
 import Email from "./pages/email";
-import SupplierDocuments from "./pages/SupplierDocuments";
 import AdminUpload from "./pages/Admin/AdminUpload";
 import Header from "./components/shared/Layout/Header";
-import DonorHome from "./pages/Admin/donorHome";
+import DonorHome from "./pages/Admin/SupplierHome";
 import WeatherForecast from './components/weatherforecast/WeatherForecast';
 import CropSalesAnalytics1 from "./pages/Dashboard/CropSalesAnalytics1";
-import DiseasePrediction from "./pages/DiseasePrediction";
 import CropPrediction from './pages/CropPrediction';
 
 
@@ -64,21 +53,14 @@ function App() {
           }
         />
         <Route
-          path="/donorhome"
+          path="/Supplierhome"
           element={
             <ProtectedRoute>
-              <DonorHome />
+              <SupplierHome />
             </ProtectedRoute>
           }
         />
-        <Route
-          path="/donar-list"
-          element={
-            <ProtectedRoute>
-              <DonarList />
-            </ProtectedRoute>
-          }
-        />
+        
         <Route
           path="/home"
           element={
@@ -89,78 +71,15 @@ function App() {
             </>
           }
         />
+        
         <Route
-          path="/hospital-list"
-          element={
-            <ProtectedRoute>
-              <HospitalList />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/org-list"
-          element={
-            <ProtectedRoute>
-              <OrgList />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/hospital"
-          element={
-            <ProtectedRoute>
-              <Hospitals />
-            </ProtectedRoute>
-          }
-        />
-        {/* <Route
           path="/analytics"
           element={
-            <ProtectedRoute>
               <Analytics />
-            </ProtectedRoute>
           }
-        /> */}
-         {/* <Route
-          path="/analytics"
-          element={
-            <>
-            <AboutUs/>
-            </>
-          }
-        /> */}
-        <Route
-          path="/consumer"
-          element={
-            <ProtectedRoute>
-              <Consumer />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/donation"
-          element={
-            <ProtectedRoute>
-              <Donation />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/organisation"
-          element={
-            <ProtectedRoute>
-              <OrganisationPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/donar"
-          element={
-            <ProtectedRoute>
-              <Donar />
-            </ProtectedRoute>
-          }
-        />
+        /> 
+        
+        <
         <Route
           path="/"
           element={
@@ -185,16 +104,7 @@ function App() {
             </PublicRoute>
           }
         />
-        <Route
-          path="/aboutus"
-          element={
-            <>
-              <Header />
-              <CropSalesAnalytics1 />
-              <Footer />
-            </>
-          }
-        />
+        
         <Route
           path="/contactus"
           element={
@@ -209,7 +119,7 @@ function App() {
           path="/weather-forecast"
           element={<WeatherForecast />}
         />
-          <Route path="/disease-prediction" element={<DiseasePrediction />} />
+         
         <Route
           path="/crop-prediction"
           element={
